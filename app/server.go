167c6138ee0ce8c702492a2a550ef636923609bf
Lenique Noralez <leniquen_services+github@protonmail.com>
2022-12-12 14:04:28 -0600

    ping

--- app/server.go
+++ app/server.go
@@ -9,14 +9,30 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	defer listener.Close()
+
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	go handleRequest(conn)
+}
+
+func handleRequest(conn net.Conn) {
+
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		os.Exit(1)
+	}
+	conn.Write([]byte("+PONG\r\n"))
 }
