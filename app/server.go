709b77982c6d35f2836a20622b3c190fe8a1f4b4
Lenique Noralez <leniquen_services+github@protonmail.com>
2022-12-14 18:13:30 -0600

    multiple pings

--- app/server.go
+++ app/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -16,34 +18,45 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-
 	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 	defer conn.Close()
-	handleRequest(conn)
-}
+	for {
 
-func handleRequest(conn net.Conn) {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
 
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
+		re := regexp.MustCompile(`\$[0-9]+\r\n`)
+		bufferString := bytes.NewBuffer(buf).String()
+		args := re.Split(bufferString, -1)
 
-	data := strings.Split(bytes.NewBuffer(buf).String(), "$4\r\n")
-	message := strings.TrimSpace(strings.ToLower(data[1]))
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-		os.Exit(1)
+		if len(args) == 0 {
+			break
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			os.Exit(1)
+		}
+
+		message := strings.TrimSpace(strings.ToLower(args[1]))
+
+		if message == "ping" && len(args) > 2 {
+			var value = "+" + strings.TrimSpace(strings.ToLower(args[2])) + "\r\n"
+			conn.Write([]byte(value))
+		} else {
+			conn.Write([]byte("+PONG\r\n"))
+		}
 	}
 
-	if message == "ping" && len(data) > 2 {
-		var value = "+" + strings.TrimSpace(strings.ToLower(data[2])) + "\r\n"
-		conn.Write([]byte(value))
-	} else {
-		conn.Write([]byte("+PONG\r\n"))
-	}
-
-	conn.Close()
 }
+
+// func handleRequest(conn net.Conn) {
+
+// 	conn.Close()
+// }
